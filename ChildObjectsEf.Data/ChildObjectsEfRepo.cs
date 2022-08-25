4f9b742d64b380a6672c53d9f35588f577f5dc34
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2022-08-25 13:07:57 +0530

    Clean up

--- ChildObjectsEf.Data/ChildObjectsEfRepo.cs
+++ ChildObjectsEf.Data/ChildObjectsEfRepo.cs
@@ -29,31 +29,6 @@ public class ChildObjectsEfRepo : IChildObjectsEfRepo
         return newOrder.GetCurrentId();
     }
 
-    void IChildObjectsEfRepo.AddItemInOrder(int orderId, string itemName, int itemQuantity)
-    {
-        throw new NotImplementedException();
-    }
-
-    void IChildObjectsEfRepo.UpdateItemNameInOrder(int orderId, int itemId, string itemName)
-    {
-        throw new NotImplementedException();
-    }
-
-    void IChildObjectsEfRepo.UpdateItemQuantityInOrder(int orderId, int itemId, int quantity)
-    {
-        throw new NotImplementedException();
-    }
-
-    void IChildObjectsEfRepo.RemoveItemFromOrder(int orderId, int itemId)
-    {
-        throw new NotImplementedException();
-    }
-
-    void IChildObjectsEfRepo.DeleteOrder(int orderId)
-    {
-        throw new NotImplementedException();
-    }
-
     async Task IChildObjectsEfRepo.SaveAllAsync()
     {
         await _childObjectsEfContext.SaveChangesAsync();
--- ChildObjectsEf.Domain/IChildObjectsEfRepo.cs
+++ ChildObjectsEf.Domain/IChildObjectsEfRepo.cs
@@ -6,15 +6,5 @@ public interface IChildObjectsEfRepo
 
     public Task<int> CreateOrderAsync(Order order);
 
-    public void AddItemInOrder(int orderId, string itemName, int itemQuantity);
-
-    public void UpdateItemNameInOrder(int orderId, int itemId, string itemName);
-
-    public void UpdateItemQuantityInOrder(int orderId, int itemId, int quantity);
-
-    public void RemoveItemFromOrder(int orderId, int itemId);
-
-    public void DeleteOrder(int orderId);
-
     public Task SaveAllAsync();    
 }
