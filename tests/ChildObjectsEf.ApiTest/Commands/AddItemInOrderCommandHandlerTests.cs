12e5feabe588a744c508d80d3fe8cd8dc1f816e7
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2022-08-26 09:52:29 +0530

    Order item validation

--- tests/ChildObjectsEf.ApiTest/Commands/AddItemInOrderCommandHandlerTests.cs
+++ tests/ChildObjectsEf.ApiTest/Commands/AddItemInOrderCommandHandlerTests.cs
@@ -40,4 +40,85 @@ public class AddItemInOrderCommandHandlerTests
         Assert.True(isSuccess);
         Assert.Contains(itemName, order.Items.Select(i => i.Name));
     }
+
+    [Fact]
+    public async Task Can_AddItemInOrderCommandHandler_ThrowException_WithNullItemName()
+    {
+        // Arrange        
+#pragma warning disable CS8600
+        string itemName = null;
+#pragma warning restore CS8600
+
+        int itemQuantity = Randomizer<int>.Create();
+        int orderId = Randomizer<int>.Create();
+
+        AddItemInOrderCommand addItemInOrderCommand = new(orderId, itemName!, itemQuantity);
+        CancellationToken cancellationToken = new();
+        Mock<IChildObjectsEfRepo> childObjectsEfRepoMock = new();
+
+        IRequestHandler<AddItemInOrderCommand, bool> requestHandler =
+            new AddItemInOrderCommandHandler(childObjectsEfRepoMock.Object);
+
+        DateTime orderDateTime = Randomizer<DateTime>.Create();
+        Order order = new(orderDateTime);
+        order.GetType().GetProperty("Id")!.SetValue(order, orderId);
+
+        childObjectsEfRepoMock
+            .Setup(s => s.GetOrderAsync(orderId))
+            .ReturnsAsync(order);
+
+        // Act
+        await Assert.ThrowsAsync<ArgumentNullException>(
+            () => requestHandler.Handle(addItemInOrderCommand, cancellationToken));
+
+        // Assert
+        childObjectsEfRepoMock
+            .Verify(v => v.GetOrderAsync(orderId),
+                Times.Once);
+
+        childObjectsEfRepoMock
+            .Verify(v => v.SaveAllAsync(),
+                Times.Never);
+
+        Assert.DoesNotContain(itemName, order.Items.Select(i => i.Name));
+    }
+
+    [Fact]
+    public async Task Can_AddItemInOrderCommandHandler_ThrowException_WithZeroItemQuantity()
+    {
+        // Arrange
+        string itemName = Randomizer<string>.Create();
+        int itemQuantity = 0;
+        int orderId = Randomizer<int>.Create();
+
+        AddItemInOrderCommand addItemInOrderCommand = new(orderId, itemName!, itemQuantity);
+        CancellationToken cancellationToken = new();
+        Mock<IChildObjectsEfRepo> childObjectsEfRepoMock = new();
+
+        IRequestHandler<AddItemInOrderCommand, bool> requestHandler =
+            new AddItemInOrderCommandHandler(childObjectsEfRepoMock.Object);
+
+        DateTime orderDateTime = Randomizer<DateTime>.Create();
+        Order order = new(orderDateTime);
+        order.GetType().GetProperty("Id")!.SetValue(order, orderId);
+
+        childObjectsEfRepoMock
+            .Setup(s => s.GetOrderAsync(orderId))
+            .ReturnsAsync(order);
+
+        // Act
+        await Assert.ThrowsAsync<ArgumentOutOfRangeException>(
+            () => requestHandler.Handle(addItemInOrderCommand, cancellationToken));
+
+        // Assert
+        childObjectsEfRepoMock
+            .Verify(v => v.GetOrderAsync(orderId),
+                Times.Once);
+
+        childObjectsEfRepoMock
+            .Verify(v => v.SaveAllAsync(),
+                Times.Never);
+
+        Assert.DoesNotContain(itemName, order.Items.Select(i => i.Name));
+    }
 }
