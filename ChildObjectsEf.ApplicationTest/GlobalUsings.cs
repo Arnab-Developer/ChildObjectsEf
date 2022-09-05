feeb6217f20cf2a796fc2b696194026b9758d244
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2022-09-05 13:19:36 +0530

    application test global using

---
+++ ChildObjectsEf.ApplicationTest/GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿global using ChildObjectsEf.Application.Commands;
+global using ChildObjectsEf.Application.Handlers;
+global using ChildObjectsEf.Domain.AggregatesModel.OrderAggregate;
+global using ChildObjectsEf.Domain.SeedData;
+global using MediatR;
+global using Moq;
+global using Tynamix.ObjectFiller;
+global using Xunit;
