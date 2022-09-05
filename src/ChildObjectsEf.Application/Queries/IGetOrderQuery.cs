1a33c0fbffeb307f13fa8914c4f021533754c611
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2022-09-05 19:04:28 +0530

    get order query test

---
+++ src/ChildObjectsEf.Application/Queries/IGetOrderQuery.cs
@@ -0,0 +1,6 @@
+﻿namespace ChildObjectsEf.Application.Queries;
+
+public interface IGetOrderQuery
+{
+    public Task<DTOs::Order> GetOrderAsync(int orderId);
+}
--- tests/ChildObjectsEf.DomainTest/AggregatesModel/OrderAggregate/OrderAggregateTests.cs
+++ tests/ChildObjectsEf.DomainTest/AggregatesModel/OrderAggregate/OrderAggregateTests.cs
@@ -5,23 +5,30 @@ public class OrderAggregateTests
     [Fact]
     public void Can_OrderAggregate_CreateOrder()
     {
+        // Arrange
         DateTime orderDate = Randomizer<DateTime>.Create();
 
+        // Act
         Order order = new(orderDate);
 
+        // Assert
         Assert.Equal(orderDate, order.OrderDate);
     }
 
     [Fact]
     public void Can_OrderAggregate_AddItemInOrder()
     {
+        // Arrange
         DateTime orderDate = Randomizer<DateTime>.Create();
         string itemName = Randomizer<string>.Create();
         int itemQuantity = Randomizer<int>.Create();
 
         Order order = new(orderDate);
+
+        // Act
         order.AddItem(itemName, itemQuantity);
 
+        // Assert
         Assert.Equal(orderDate, order.OrderDate);
         Assert.Equal(itemName, order.Items.ElementAt(0).Name);
         Assert.Equal(itemQuantity, order.Items.ElementAt(0).Quantity);
@@ -30,6 +37,7 @@ public class OrderAggregateTests
     [Fact]
     public void Can_OrderAggregate_ThroughException_WithNullItemName()
     {
+        // Arrange
         DateTime orderDate = Randomizer<DateTime>.Create();
 
 #pragma warning disable CS8600
@@ -37,10 +45,12 @@ public class OrderAggregateTests
 #pragma warning restore CS8600
 
         int itemQuantity = Randomizer<int>.Create();
-
         Order order = new(orderDate);
+
+        // Act
         Assert.Throws<ArgumentNullException>(() => order.AddItem(itemName!, itemQuantity));
 
+        // Assert
         Assert.Equal(orderDate, order.OrderDate);
         Assert.Empty(order.Items);
     }
@@ -48,13 +58,17 @@ public class OrderAggregateTests
     [Fact]
     public void Can_OrderAggregate_ThroughException_WithZeroItemQuantity()
     {
+        // Arrange
         DateTime orderDate = Randomizer<DateTime>.Create();
         string itemName = Randomizer<string>.Create();
         int itemQuantity = 0;
 
         Order order = new(orderDate);
+
+        // Act
         Assert.Throws<ArgumentOutOfRangeException>(() => order.AddItem(itemName, itemQuantity));
 
+        // Assert
         Assert.Equal(orderDate, order.OrderDate);
         Assert.Empty(order.Items);
     }
@@ -62,6 +76,7 @@ public class OrderAggregateTests
     [Fact]
     public void Can_OrderAggregate_UpdateItemName()
     {
+        // Arrange
         DateTime orderDate = Randomizer<DateTime>.Create();
         string itemName = Randomizer<string>.Create();
         int itemQuantity = Randomizer<int>.Create();
@@ -73,14 +88,17 @@ public class OrderAggregateTests
         OrderItem item = order.Items.First(i => i.Name == itemName);
         item.GetType().GetProperty("Id")!.SetValue(item, itemId);
 
+        // Act
         order.UpdateItemName(itemId, "updated name");
 
+        // Assert
         Assert.Equal("updated name", order.Items.ElementAt(0).Name);
     }
 
     [Fact]
     public void Can_OrderAggregate_UpdateItemQuantity()
     {
+        // Arrange
         DateTime orderDate = Randomizer<DateTime>.Create();
         string itemName = Randomizer<string>.Create();
         int itemQuantity = Randomizer<int>.Create();
@@ -92,14 +110,17 @@ public class OrderAggregateTests
         OrderItem item = order.Items.First(i => i.Name == itemName);
         item.GetType().GetProperty("Id")!.SetValue(item, itemId);
 
+        // Act
         order.UpdateItemQuantity(itemId, 105);
 
+        // Assert
         Assert.Equal(105, order.Items.ElementAt(0).Quantity);
     }
 
     [Fact]
     public void Can_OrderAggregate_RemoveItemFromOrder()
     {
+        // Arrange
         DateTime orderDate = Randomizer<DateTime>.Create();
         string itemName = Randomizer<string>.Create();
         int itemQuantity = Randomizer<int>.Create();
@@ -111,8 +132,10 @@ public class OrderAggregateTests
         OrderItem item = order.Items.First(i => i.Name == itemName);
         item.GetType().GetProperty("Id")!.SetValue(item, itemId);
 
+        // Act
         order.RemoveItem(itemId);
 
+        // Assert
         Assert.Empty(order.Items);
     }
 }
