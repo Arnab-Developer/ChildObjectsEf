ef25d2bb6f6cb5e0d5403250c63ef2146dc619e8
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2022-09-09 10:29:22 +0530

    test update for multiple orders return

--- tests/ChildObjectsEf.ApplicationTest/Queries/GetOrderByDateQueryTests.cs
+++ tests/ChildObjectsEf.ApplicationTest/Queries/GetOrderByDateQueryTests.cs
@@ -22,6 +22,16 @@ public class GetOrderByDateQueryTests
                     new DTOs::OrderItem() { Id = 1, Name = "item 1", Quantity = 100 },
                     new DTOs::OrderItem() { Id = 2, Name = "item 2", Quantity = 254 }
                 }
+            },
+            new DTOs::Order()
+            {
+                Id = 2,
+                OrderDate = DateTime.Now,
+                Items = new List<DTOs::OrderItem>()
+                {
+                    new DTOs::OrderItem() { Id = 3, Name = "item 3", Quantity = 1478 },
+                    new DTOs::OrderItem() { Id = 4, Name = "item 4", Quantity = 8574 }
+                }
             }
         };
 
@@ -47,5 +57,16 @@ public class GetOrderByDateQueryTests
         Assert.Equal(2, orders[0].Items.ElementAt(1).Id);
         Assert.Equal("item 2", orders[0].Items.ElementAt(1).Name);
         Assert.Equal(254, orders[0].Items.ElementAt(1).Quantity);
+
+        Assert.Equal(2, orders[1].Id);
+        Assert.Equal(DateTime.Now.Date, orders[1].OrderDate.Date);
+
+        Assert.Equal(3, orders[1].Items.ElementAt(0).Id);
+        Assert.Equal("item 3", orders[1].Items.ElementAt(0).Name);
+        Assert.Equal(1478, orders[1].Items.ElementAt(0).Quantity);
+
+        Assert.Equal(4, orders[1].Items.ElementAt(1).Id);
+        Assert.Equal("item 4", orders[1].Items.ElementAt(1).Name);
+        Assert.Equal(8574, orders[1].Items.ElementAt(1).Quantity);
     }
 }
