f4a70ea72a9549ee521712ec79ac943fd861ace6
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2022-09-11 11:19:53 +0530

    remove order test

--- tests/ChildObjectsEf.DataTest/ChildObjectsEfRepoTests.cs
+++ tests/ChildObjectsEf.DataTest/ChildObjectsEfRepoTests.cs
@@ -111,6 +111,64 @@ public class ChildObjectsEfRepoTests
         DropDb();
     }
 
+    [Fact]
+    public async Task Can_DeleteOrder_DeleteProperData()
+    {
+        DateTime orderDateTime = Randomizer<DateTime>.Create();
+        int orderId = Randomizer<int>.Create();
+
+        Order order = new(orderDateTime);
+        order.AddItem("item1", 10);
+        order.AddItem("item2", 20);
+
+        order.GetType().GetProperty("Id")!.SetValue(order, orderId);
+
+        OrderItem item = order.Items.First(i => i.Name == "item1");
+        item.GetType().GetProperty("Id")!.SetValue(item, 1);
+
+        item = order.Items.First(i => i.Name == "item2");
+        item.GetType().GetProperty("Id")!.SetValue(item, 2);
+
+        DbContextOptions<ChildObjectsEfContext> arrangeOptions = new DbContextOptionsBuilder<ChildObjectsEfContext>()
+            .UseInMemoryDatabase("ChildObjectsEfDb")
+            .Options;
+
+        using (ChildObjectsEfContext arrangeContext = new(arrangeOptions))
+        {
+            await arrangeContext.Orders.AddAsync(order);
+            await arrangeContext.SaveChangesAsync();
+        }
+
+        DbContextOptions<ChildObjectsEfContext> options = new DbContextOptionsBuilder<ChildObjectsEfContext>()
+            .UseInMemoryDatabase("ChildObjectsEfDb")
+            .Options;
+
+        using (ChildObjectsEfContext context = new(options))
+        {
+            IChildObjectsEfRepo childObjectsEfRepo = new ChildObjectsEfRepo(context);
+
+            // Act
+            childObjectsEfRepo.DeleteOrder(order);
+            await childObjectsEfRepo.UnitOfWork.SaveChangesAsync();
+        }
+
+        // Assert
+        DbContextOptions<ChildObjectsEfContext> assertOptions = new DbContextOptionsBuilder<ChildObjectsEfContext>()
+            .UseInMemoryDatabase("ChildObjectsEfDb")
+            .Options;
+
+        using ChildObjectsEfContext assertContext = new(assertOptions);
+
+        Order? dbOrder = await assertContext.Orders
+            .Include(o => o.Items)
+            .FirstOrDefaultAsync(o => o.OrderDate == orderDateTime);
+
+        Assert.Null(dbOrder);
+
+        // Clean up
+        DropDb();
+    }
+
     private static void DropDb()
     {
         DbContextOptions<ChildObjectsEfContext> options = new DbContextOptionsBuilder<ChildObjectsEfContext>()
