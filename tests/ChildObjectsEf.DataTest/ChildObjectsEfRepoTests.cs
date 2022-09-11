3f38c8f727a7f1e3c994f510cd300bbdd3a211bc
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2022-09-11 11:06:29 +0530

    drop db after every test

--- tests/ChildObjectsEf.DataTest/ChildObjectsEfRepoTests.cs
+++ tests/ChildObjectsEf.DataTest/ChildObjectsEfRepoTests.cs
@@ -25,16 +25,17 @@ public class ChildObjectsEfRepoTests
             .UseInMemoryDatabase("ChildObjectsEfDb")
             .Options;
 
-        ChildObjectsEfContext arrangeContext = new(arrangeOptions);
-
-        await arrangeContext.Orders.AddAsync(order);
-        await arrangeContext.SaveChangesAsync();
+        using (ChildObjectsEfContext arrangeContext = new(arrangeOptions))
+        {
+            await arrangeContext.Orders.AddAsync(order);
+            await arrangeContext.SaveChangesAsync();
+        }
 
         DbContextOptions<ChildObjectsEfContext> options = new DbContextOptionsBuilder<ChildObjectsEfContext>()
             .UseInMemoryDatabase("ChildObjectsEfDb")
             .Options;
 
-        ChildObjectsEfContext context = new(options);
+        using ChildObjectsEfContext context = new(options);
         IChildObjectsEfRepo childObjectsEfRepo = new ChildObjectsEfRepo(context);
 
         // Act
@@ -49,5 +50,74 @@ public class ChildObjectsEfRepoTests
 
         Assert.Equal("item2", dbOrder.Items.First(i => i.Id == 2).Name);
         Assert.Equal(20, dbOrder.Items.First(i => i.Id == 2).Quantity);
+
+        // Clean up
+        DropDb();
+    }
+
+    [Fact]
+    public async Task Can_CreateOrderAsync_ReturnProperData()
+    {
+        // Arrange
+        DateTime orderDateTime = Randomizer<DateTime>.Create();
+        int orderId = Randomizer<int>.Create();
+
+        Order order = new(orderDateTime);
+        order.AddItem("item1", 10);
+        order.AddItem("item2", 20);
+
+        order.GetType().GetProperty("Id")!.SetValue(order, orderId);
+
+        OrderItem item = order.Items.First(i => i.Name == "item1");
+        item.GetType().GetProperty("Id")!.SetValue(item, 1);
+
+        item = order.Items.First(i => i.Name == "item2");
+        item.GetType().GetProperty("Id")!.SetValue(item, 2);
+
+        DbContextOptions<ChildObjectsEfContext> options = new DbContextOptionsBuilder<ChildObjectsEfContext>()
+            .UseInMemoryDatabase("ChildObjectsEfDb")
+            .Options;
+
+        using (ChildObjectsEfContext context = new(options))
+        {
+            IChildObjectsEfRepo childObjectsEfRepo = new ChildObjectsEfRepo(context);
+
+            // Act
+            _ = await childObjectsEfRepo.CreateOrderAsync(order);
+            await childObjectsEfRepo.UnitOfWork.SaveChangesAsync();
+        }
+
+        // Assert
+        DbContextOptions<ChildObjectsEfContext> assertOptions = new DbContextOptionsBuilder<ChildObjectsEfContext>()
+            .UseInMemoryDatabase("ChildObjectsEfDb")
+            .Options;
+
+        using ChildObjectsEfContext assertContext = new(options);
+
+        Order dbOrder = await assertContext.Orders
+            .Include(o => o.Items)
+            .FirstAsync(o => o.OrderDate == orderDateTime);
+
+        Assert.Equal(orderId, dbOrder.Id);
+        Assert.Equal(orderDateTime, dbOrder.OrderDate);
+
+        Assert.Equal("item1", dbOrder.Items.First(i => i.Id == 1).Name);
+        Assert.Equal(10, dbOrder.Items.First(i => i.Id == 1).Quantity);
+
+        Assert.Equal("item2", dbOrder.Items.First(i => i.Id == 2).Name);
+        Assert.Equal(20, dbOrder.Items.First(i => i.Id == 2).Quantity);
+
+        // Clean up
+        DropDb();
+    }
+
+    private static void DropDb()
+    {
+        DbContextOptions<ChildObjectsEfContext> options = new DbContextOptionsBuilder<ChildObjectsEfContext>()
+            .UseInMemoryDatabase("ChildObjectsEfDb")
+            .Options;
+
+        using ChildObjectsEfContext context = new(options);
+        context.Database.EnsureDeleted();
     }
 }
